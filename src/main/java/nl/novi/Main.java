d89d7498f99055d5349a31ab33a33c340c2db843
DanaSnellens <danasnellens@gmail.com>
2023-10-14 00:40:14 +0200

    Huiswerkklas meegedaan & maven toegevoegd. Nog verder invullen pom.xml

---
+++ src/main/java/nl/novi/Main.java
@@ -0,0 +1,11 @@
+package nl.novi;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person mother = new Person("Birgit", "Snellens", "vrouw", 62);
+        Person father = new Person("Nigel", "Harle", "man", 71);
+        Person me = new Person("Dana", "Snellens", "vrouw", 35);
+
+    }
+}
